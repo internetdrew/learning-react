4f20daf6e7f06bf155c04d7d63b77078df0681ef
Andrew Rowley <andrew@internetdrew.com>
2022-10-23 16:28:05 -0400

    create book components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -4,22 +4,34 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<BookList />);
 
-// JSX Rules
-// return single element
-// div / section / article or Fragment
-// use camelCase for html attribute
-// className instead of class
-// close every element
-// formatting
-
 function BookList() {
   return (
     <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
       <Book />
     </section>
   );
 }
 
 const Book = () => {
-  return <article>this is a book</article>;
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
 };
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/618INpEnVjL._AC_UL900_SR900,600_.jpg'
+    alt=''
+  />
+);
+
+const Title = () => <h1>Spooky Pookie (Little Pookie)</h1>;
+const Author = () => <h4>Sandra Boynton</h4>;
