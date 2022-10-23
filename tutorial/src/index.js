d738fc7c6bf00e64739a7dec056ea77e2c271a2e
Andrew Rowley <andrew@internetdrew.com>
2022-10-23 18:15:42 -0400

    Improve variables and book component structure

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<BookList />);
 
+const author = 'Sandra Boynton';
+const title = 'Spooky Pookie (Little Pookie)';
+const img =
+  'https://m.media-amazon.com/images/I/41VsfjWEvSL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg';
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
       <Book />
       <Book />
     </section>
@@ -21,19 +23,11 @@ function BookList() {
 const Book = () => {
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt='' />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {/* <p>{let x = 6}</p> */}
+      <p>{6 + 6}</p>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src='https://m.media-amazon.com/images/I/41VsfjWEvSL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
-    alt=''
-  />
-);
-
-const Title = () => <h1>Spooky Pookie (Little Pookie)</h1>;
-const Author = () => <h4>Sandra Boynton</h4>;
