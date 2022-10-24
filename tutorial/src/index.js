16fa3107f6ffa7126f1f1d5ff883868b069af466
Andrew Rowley <andrew@internetdrew.com>
2022-10-24 08:24:33 -0400

    test event handling syntax and scenarios

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -31,17 +31,32 @@ function BookList() {
   );
 }
 
-const Book = props => {
-  console.log(props);
-  const { img, title, author } = props;
-
+const Book = ({ img, title, author }) => {
+  const clickHandler = e => {
+    console.log(e);
+    alert('hello world');
+  };
+  const complexEx = author => {
+    console.log(author);
+  };
   return (
-    <article className='book'>
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt='' />
       <div>
         <h1>{title}</h1>
         <h4>{author}</h4>
       </div>
+      <button type='button' onClick={clickHandler}>
+        reference example
+      </button>
+      <button type='button' onClick={() => complexEx(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
