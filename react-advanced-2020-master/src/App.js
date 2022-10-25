16cfb3af6812f1f76347b97bb8e1ccd56e2432e0
Andrew Rowley <andrew@internetdrew.com>
2022-10-25 11:47:04 -0400

    render birthday reminder project starter code

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/3-useState-array';
+// import Setup from './tutorial/1-useState/setup/3-useState-array';
+import Project from './projects/01-birthday-reminder/setup/src/App';
 
 function App() {
   return (
     <div className='container'>
-      <Setup />
+      {/* <Setup /> */}
+      <Project />
     </div>
   );
 }
