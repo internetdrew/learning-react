2f664ff3122c819a4f4422ed08e06979223a1452
Andrew Rowley <andrew@internetdrew.com>
2022-10-24 22:05:27 -0400

    cover the basics of useState

--- react-advanced-2020-master/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020-master/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState('random title');
+  const handleClick = () => {
+    text === 'random title' ? setText('hello world') : setText('random title');
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className='btn' onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
