8393918ee6ffa3a3953602369e17b205fa047cef
Andrew Rowley <andrew@internetdrew.com>
2022-10-28 13:34:29 -0400

    add tours and deletion functionality

--- react-advanced-2020-master/src/projects/02-tours/setup/src/App.js
+++ react-advanced-2020-master/src/projects/02-tours/setup/src/App.js
@@ -1,11 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = id => {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    try {
+      const res = await fetch(url);
+      if (!res.ok) throw new Error('problem fetching');
+      const tours = await res.json();
+      console.log(tours);
+      setLoading(false);
+      setTours(tours);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
