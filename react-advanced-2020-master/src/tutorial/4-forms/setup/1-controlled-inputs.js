996076940cd3da8e52379e29f3addff6bbe7a1bc
Andrew Rowley <andrew@internetdrew.com>
2022-11-04 12:22:47 -0400

    complete simple form

--- react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-2020-master/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,48 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(firstName, email);
+    setFirstName('');
+    setEmail('');
+  };
+
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name:</label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              placeholder='Enter your name...'
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email:</label>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              placeholder='Enter your email address...'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <button type='submit'>add person</button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
