82325d6740adc1937781f8d4f85e4f75e08638c6
Andrew Rowley <andrew@internetdrew.com>
2022-11-06 00:13:18 -0400

    add editing functionality

--- react-advanced-2020-master/src/projects/10-grocery-bud/setup/src/App.js
+++ react-advanced-2020-master/src/projects/10-grocery-bud/setup/src/App.js
@@ -22,6 +22,19 @@ function App() {
 
     if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map(item => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
+      return;
     }
 
     // show alert
@@ -48,6 +61,13 @@ function App() {
     setList(list.filter(item => item.id !== id));
   };
 
+  const editItem = id => {
+    const specificItem = list.find(item => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -68,7 +88,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
