bd4ccea810f0833a2f4c241d305c38336b8a5640
Andrew Rowley <andrew@internetdrew.com>
2022-11-06 23:01:41 -0500

    introduce useReducer for refactoring

--- react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -2,11 +2,19 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = (state, action) => {};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: 'hello world',
+};
 
 const Index = () => {
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = e => {
     e.preventDefault();
