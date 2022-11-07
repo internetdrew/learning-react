225cfbbd8ec27fb465b9ffa23b00d7355b0ba930
Andrew Rowley <andrew@internetdrew.com>
2022-11-06 22:54:42 -0500

    implement functionality to add names to list

--- react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,48 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (!name.trim()) {
+      setShowModal(true);
+      return;
+    }
+
+    if (name.trim()) {
+      setShowModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName('');
+    }
+  };
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className='form'>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <button type='submit'>add person</button>
+      </form>
+      {people.map(person => {
+        const { name, id } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
