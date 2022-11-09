aa2cdd73098de416f1ed89b9be8ad3469769604f
Andrew Rowley <andrew@internetdrew.com>
2022-11-09 10:35:08 -0500

    create context and functions for sidebar and submenu

--- react-advanced-2020-master/src/projects/13-stripe-submenus/setup/src/context.js
+++ react-advanced-2020-master/src/projects/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,30 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [IsSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => setIsSidebarOpen(true);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
+  const openSubmenu = () => setIsSubmenuOpen(true);
+  const closeSubmenu = () => setIsSubmenuOpen(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        IsSidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
