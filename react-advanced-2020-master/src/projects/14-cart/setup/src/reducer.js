1fc7ac65cadc2af620e394f1b1cee19860140f9b
Andrew Rowley <andrew@internetdrew.com>
2022-11-14 06:28:33 -0500

    increase and decrease item totals

--- react-advanced-2020-master/src/projects/14-cart/setup/src/reducer.js
+++ react-advanced-2020-master/src/projects/14-cart/setup/src/reducer.js
@@ -9,6 +9,27 @@ const reducer = (state, action) => {
       cart: state.cart.filter(item => item.id !== action.payload),
     };
   }
+
+  if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map(cartItem => {
+      return cartItem.id === action.payload
+        ? { ...cartItem, amount: cartItem.amount + 1 }
+        : cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === 'DECREASE') {
+    let tempCart = state.cart
+      .map(cartItem => {
+        return cartItem.id === action.payload
+          ? { ...cartItem, amount: cartItem.amount - 1 }
+          : cartItem;
+      })
+      .filter(cartItem => cartItem.amount > 0);
+    return { ...state, cart: tempCart };
+  }
+
   return state;
 };
 
