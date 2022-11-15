fd1be4cae619c609690b2bd3c30073dc16f2bc22
Andrew Rowley <andrew@internetdrew.com>
2022-11-15 16:38:24 -0500

    pull cocktails and restructure data from api

--- react-advanced-2020-master/src/projects/15-cocktails/setup/src/pages/About.js
+++ react-advanced-2020-master/src/projects/15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet in
+        possimus odit dicta ipsa cumque optio explicabo saepe perspiciatis
+        exercitationem iste ab est quod nostrum, laudantium eligendi nulla
+        aliquam laboriosam nobis? Similique perferendis architecto placeat
+        dolores, et odio quae exercitationem atque, at, quidem sunt?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
